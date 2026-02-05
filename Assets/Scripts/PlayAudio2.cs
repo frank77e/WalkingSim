0470f0c225f355351ac0720e807a19bf9f26d085
frank77e <dthoma30@pratt.edu>
2026-02-05 16:44:13 -0500

    added skybox

--- Assets/Scripts/PlayAudio2.cs
+++ Assets/Scripts/PlayAudio2.cs
@@ -1,25 +1,39 @@
-using System.Collections;
+
 using UnityEngine;
 using UnityEngine.Audio;
 
-[RequireComponent(typeof(AudioSource))]
 public class PlayAudio2 : MonoBehaviour
 {
-    public float fadeTimeInSeconds;
-    public 
+    public AudioMixerSnapshot unmutedSnapshot;
+    public AudioMixerSnapshot mutedSnapshot;
+    public float fadeTime;
 
     private AudioSource audio;
     private AudioMixer mixer;
+    private float[] weights;
+    private AudioMixerSnapshot[] snapshots;
     void Start()
     {
         audio = GetComponent<AudioSource>();
+        mixer = audio.outputAudioMixerGroup.audioMixer;
+
+        snapshots = new AudioMixerSnapshot[]
+        {
+            unmutedSnapshot, //index 0
+            mutedSnapshot       //index 1
+        };
+
+        // make an array with 2 values
+        weights = new float[2];
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-
+            weights[0] = 1;
+            weights[1] = 0;
+            mixer.TransitionToSnapshots(snapshots, weights, fadeTime);
         }
 
     }
